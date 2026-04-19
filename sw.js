c2b8d4fa9bf27186f33cbf56be9214e91de61a4f
Harald Eikanger <126562604+HarryKure@users.noreply.github.com>
2026-04-19 21:42:25 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,15 @@
-const CACHE = 'calbank-v1';
-const FILES = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'calbank-v2';
+const FILES = ['./index.html', './manifest.json', './icon.png'];
 
 self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
 });
 
 self.addEventListener('fetch', e => {
